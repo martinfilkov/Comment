89496810969cf4bee19f6dd71722ce60c98e49e5
martinfilkov <martinfilkov1@gmail.com>
2024-07-15 11:59:32 +0300

    Remove flattened poms

---
+++ rest/src/test/java/com/tinqinacademy/comment/rest/controllers/SystemControllerTest.java
@@ -0,0 +1,65 @@
+package com.tinqinacademy.comment.rest.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tinqinacademy.comment.api.operations.system.deletecomment.DeleteCommentInput;
+import com.tinqinacademy.comment.api.operations.system.updatecomment.UpdateCommentInput;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class SystemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private SystemController systemController;
+
+    @Test
+    public void givenUpdateInput_whenUpdateComment_thenReturnUpdateCommentId() throws Exception {
+        UpdateCommentInput input = UpdateCommentInput.builder()
+                .commentId("1")
+                .firstName("Martin")
+                .lastName("Filkov")
+                .roomNumber("12B")
+                .content("CONTENT")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(input);
+
+        mockMvc.perform(put(URLMapping.UPDATE_COMMENT, input.getCommentId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.id").value("1"));
+
+    }
+
+    @Test
+    public void givenDeleteInput_whenDeleteComment_thenDeleteComment() throws Exception{
+        DeleteCommentInput input = DeleteCommentInput.builder()
+                .commentId("1")
+                .build();
+
+        String requestBody = objectMapper.writeValueAsString(input);
+
+        mockMvc.perform(delete(URLMapping.DELETE_COMMENT, input.getCommentId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isAccepted());
+    }
+}
