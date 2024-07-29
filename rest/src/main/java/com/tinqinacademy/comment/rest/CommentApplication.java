d2323d6a6e632b0915ae6823a7327a56b618b4d1
martinfilkov <martinfilkov1@gmail.com>
2024-07-29 14:02:41 +0300

    Fix entity scan

--- rest/src/main/java/com/tinqinacademy/comment/rest/CommentApplication.java
+++ rest/src/main/java/com/tinqinacademy/comment/rest/CommentApplication.java
@@ -2,9 +2,13 @@ package com.tinqinacademy.comment.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.tinqinacademy.comment.persistence.entities")
+@EnableJpaRepositories(basePackages = "com.tinqinacademy.comment.persistence.repositories")
 @ComponentScan(basePackages = "com.tinqinacademy.comment")
 public class CommentApplication {
 
