b9c9cd1e7c3ef4355059a3d512e7db22b9a19784
martinfilkov <martinfilkov1@gmail.com>
2024-07-29 12:03:38 +0300

    Remove patch update validation

--- api/src/main/java/com/tinqinacademy/comment/api/operations/system/updatecomment/AdminUpdateCommentInput.java
+++ api/src/main/java/com/tinqinacademy/comment/api/operations/system/updatecomment/AdminUpdateCommentInput.java
@@ -1,7 +1,6 @@
 package com.tinqinacademy.comment.api.operations.system.updatecomment;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @NoArgsConstructor
@@ -13,16 +12,8 @@ import lombok.*;
 public class AdminUpdateCommentInput {
     @JsonIgnore
     private String commentId;
-
-    @NotBlank(message = "Room cannot be blank")
     private String roomNumber;
-
-    @NotBlank(message = "First name cannot be blank")
     private String firstName;
-
-    @NotBlank(message = "Last name cannot be blank")
     private String lastName;
-
-    @NotBlank(message = "Content cannot be blank")
     private String content;
 }
